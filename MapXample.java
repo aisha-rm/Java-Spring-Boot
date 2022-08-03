e86d9f4fc0b125a637f28ba33045bb54275315c3
Aisha Mohammed <80531561+aisha-rm@users.noreply.github.com>
2022-08-03 21:30:35 +0100

    new commit

---
+++ MapXample.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.TreeMap;
+
+public class MapXample {
+
+    //count the frequency of occurence of a string in a list of strings
+
+    public static void calculateWordFrequency(List<String> input){
+        TreeMap<String, Integer> frequencyMap = new TreeMap<>();  //to hold each string in the input and number of occurence
+        //now iterating through the list of strings 
+        for (String str : input){
+            frequencyMap.put(str, frequencyMap.getOrDefault(str, 0) + 1);
+        }
+        System.out.println("The map: " + frequencyMap);
+        System.out.println("The set: " + frequencyMap.keySet());
+
+        for (String word : frequencyMap.keySet()){
+            System.out.println(word + "|" + frequencyMap.get(word));
+        }
+    }
+    
+    public static void main(String[] args){
+        calculateWordFrequency(Arrays.asList("Amina", "Aisha", "Mohammed","Amina", "Amina", "Mustapha", "Aisha"));
+    }
+}
