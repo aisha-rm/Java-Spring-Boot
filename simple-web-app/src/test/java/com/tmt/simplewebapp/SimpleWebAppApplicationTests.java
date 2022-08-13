9ecdf396859eebd075774937771d89c2c857eab0
Aisha Mohammed <80531561+aisha-rm@users.noreply.github.com>
2022-08-13 20:38:41 +0100

    initial commit

---
+++ simple-web-app/src/test/java/com/tmt/simplewebapp/SimpleWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tmt.simplewebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
