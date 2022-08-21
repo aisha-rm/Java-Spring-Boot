efbf5e1f3e715eaaae29be15c46859c49abc8ec8
Aisha Mohammed <80531561+aisha-rm@users.noreply.github.com>
2022-08-21 14:37:54 +0100

    initial commit

---
+++ workbook/src/main/java/com/tmt/workbook/WorkbookController.java
@@ -0,0 +1,13 @@
+package com.tmt.workbook;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WorkbookController {
+    
+    @GetMapping("/shows")
+    public String getShows(){
+        return "shows";
+    }
+}
